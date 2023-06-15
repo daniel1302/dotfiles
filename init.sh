81247a42e345fff9327419d9c552f5fbf481bbc0
daniel1302 <daniel.1302@gmail.com>
2023-06-15 20:59:56 +0200

    feat: add init.sh

--- init.sh
+++ init.sh
@@ -7,10 +7,22 @@ sudo whoami;
 go install github.com/incu6us/goimports-reviser/v3@latest ;
 go install mvdan.cc/gofumpt@latest ;
 go install github.com/segmentio/golines@latest ;
+go install github.com/go-delve/delve/cmd/dlv@latest ;
+go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.50.1 ;
+go install golang.org/x/tools/cmd/goimports@latest ;
+
 
 # rust tools
 rustup component add rust-src;
 rustup component add rust-analyzer;
+
+if ! command -v tfenv; then
+  git clone --depth=1 https://github.com/tfutils/tfenv.git ~/.tfenv ;
+  echo 'export PATH="$HOME/.tfenv/bin:$PATH"' >> ~/.profile ;
+  source ~/.profile;
+  tfenv install;
+  tfenv use;
+fi;
 
 
 # check if gradlew installed
